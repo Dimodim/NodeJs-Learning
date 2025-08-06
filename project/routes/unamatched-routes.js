1cf4af75bb2995c8050018596ff58a18865b7f99
Dimodim <dimitardddm97@gmail.com>
2025-08-06 13:24:16 +0300

    updated styles and serving static content

--- project/routes/unamatched-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.use((req, res) => {
-  res.status(404).send("<h1>404 - Not Found</h1>");
-});
-
-module.exports = router;
