3850633c53977a09bf6d3a4647c9b4b4c5835546
Dimodim <dimitardddm97@gmail.com>
2025-08-18 14:53:29 +0300

    added mysql

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
+router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
@@ -13,9 +14,12 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
-router.get('/', shopController.getIndex);
 module.exports = router;
